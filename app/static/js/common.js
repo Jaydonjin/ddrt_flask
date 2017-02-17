0e52ead991c7b276b763a3451853445955f42f85
jaydon.t.jin <jaydon.t.jin@newegg.com>
2017-02-17 13:07:55 +0800

    add html

---
+++ app/static/js/common.js
@@ -0,0 +1,61 @@
+var setCookie = function(c_name,value,expiredays){
+            var exdate=new Date()
+            exdate.setDate(exdate.getDate()+expiredays)
+            cookieVal=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+            //alert(cookieVal);
+            document.cookie=cookieVal;
+        };
+
+//get cookie
+var getCookie = function(c_name){
+    if (document.cookie.length>0){
+        c_start=document.cookie.indexOf(c_name + "=")
+    if (c_start!=-1){ 
+        c_start=c_start + c_name.length+1 
+        c_end=document.cookie.indexOf(";",c_start)
+        if (c_end==-1) c_end=document.cookie.length
+        //document.write(document.cookie.substring(c_start,c_end)+"<br>");
+        return unescape(document.cookie.substring(c_start,c_end))
+        } 
+    }
+    return ""
+};
+
+Array.prototype.remove=function(element){
+    var index = $.inArray(element, this);
+    if (index === -1) {
+        return;
+    }
+    this.splice(index, 1);
+};
+
+function getStrLen(str){
+    var realLength = 0;
+    var len = str.length;
+    var charCode = -1;
+    for(var i = 0; i < len; i++){
+        charCode = str.charCodeAt(i);
+        if (charCode >= 0 && charCode <= 128) { 
+            realLength += 1;
+        }else{ 
+            //if is chinese
+            realLength += 2;
+        }
+    } 
+    return realLength;
+};
+
+function EnEight(txt){
+    var monyer = new Array();var i,s;
+    for(i=0;i<txt.length;i++)
+        monyer+="\\"+txt.charCodeAt(i).toString(8); 
+    return monyer;
+};
+
+function DeEight(txt){
+    var monyer = new Array();var i;
+    var s=txt.split("\\");
+    for(i=1;i<s.length;i++)
+        monyer+=String.fromCharCode(parseInt(s[i],8));
+    return monyer;
+};
